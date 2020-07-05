b49ed2adf8c3d893763602e721affe5c78c14590
fabricadecolete <67875336+fabricadecolete@users.noreply.github.com>
2020-07-05 22:18:39 +0300

    Initial commit

---
+++ precache-manifest.9c15523068ace4df1a70cac293815548.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f50e7af152ce31305f22197cbd80d631",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce5bcb08db677287716c",
+    "url": "/static/css/main.802f0dad.chunk.css"
+  },
+  {
+    "revision": "c25541a488e2622cbc10",
+    "url": "/static/js/2.5868efff.chunk.js"
+  },
+  {
+    "revision": "ed901023237d66f16963aae8c7889084",
+    "url": "/static/js/2.5868efff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce5bcb08db677287716c",
+    "url": "/static/js/main.02d2afa8.chunk.js"
+  },
+  {
+    "revision": "934ad88702469c64ef35",
+    "url": "/static/js/runtime-main.cad331f0.js"
+  },
+  {
+    "revision": "588a28f46d2eb4e457cca65c9cbee700",
+    "url": "/static/media/dragonStarSkew.588a28f4.png"
+  },
+  {
+    "revision": "657706c7d85dce734ac7ce4299676ef1",
+    "url": "/static/media/heroNoCourier.657706c7.svg"
+  },
+  {
+    "revision": "e8ba017f9b31796ff253ea139dec3945",
+    "url": "/static/media/logo-white.e8ba017f.svg"
+  },
+  {
+    "revision": "bf7232e1e99a1423baf01fe70692aba7",
+    "url": "/static/media/md-check-circle.bf7232e1.svg"
+  },
+  {
+    "revision": "a63a5654c6fbdba1b867d829d959800d",
+    "url": "/static/media/nemoSkew.a63a5654.png"
+  },
+  {
+    "revision": "c25338880b1e1f60676723bbfd5d4ba3",
+    "url": "/static/media/urgentCargusSkew.c2533888.png"
+  }
+]);
